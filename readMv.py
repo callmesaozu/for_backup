0ac6e5b20f4304b55d39ba08b92398c78a5ff0b7
lloyd <yeshenghan@yeah.net>
2016-04-09 09:03:02 +0800

    编写了一个文件，从movieLen的数据集中读取数据

---
+++ readMv.py
@@ -0,0 +1,22 @@
+#python2.7.3
+import re
+def read_mv(fpath):
+  #f = open(fpath, 'r', encoding = 'lating-1')
+  f = open(fpath, 'r')
+  dct = {}
+  for line in f:
+      #matc = re.search('::.+::.*Children\'s', line)
+    matc =  re.search('::.+::(.+)', line)     
+    if matc != None:
+        gener = re.split('\|', matc.group(1))
+        print(gener)
+        for i in gener:
+            if dct.has_key(i) is False:
+                dct[i] = 1
+            else:
+                dct[i] += 1
+  f.close()
+  return dct
+          
+fpath = '//home//lord//data//ml-1m//movies.dat'
+dct = read_mv(fpath)
